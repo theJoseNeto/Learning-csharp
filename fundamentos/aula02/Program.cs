cb842ec78a9930b5ec83a6bff017a8c9688c01d0
theJoseNeto <joseneto180201@gmail.com>
2023-03-01 17:23:59 -0300

    aula 02: Recebendo argumentos por linha de comando e tomando decisões a partir disso

---
+++ fundamentos/aula02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System; 
+
+namespace aula02 
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            if(args.GetValue(0).ToString() == "red"){
+                Console.Write("is red");
+                return;
+            }
+
+            Console.Write("is not red");
+
+        }
+    }
+}
+
+ 
