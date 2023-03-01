5e8cdd50bbb480725e17548518501552f50bcf9c
theJoseNeto <joseneto180201@gmail.com>
2023-03-01 18:46:21 -0300

    aula 03: Variaveis

---
+++ fundamentos/aula03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System; 
+
+namespace aula03 
+{
+    class Program
+    {
+        static void Main(string[] args){
+            //variáveis e seus tipos mais primitivos
+            int num = 1; 
+            char letter = "w";
+            float pi = 3.14f; 
+            string name = "Neto"; 
+            var aux = name; 
+
+            // constantes 
+            const int num = 1; 
+            const char letter = "w";
+            const float pi = 3.14f; 
+            const string name = "Neto"; 
+            const var aux = name; 
+
+            // Declaração de multiplas variáveis com mesmo tipo; 
+            int num2, num3, num4;
+            char teste1, teste2, teste3; 
+            // ... 
+
+                
+            
+        }
+    }
+}
