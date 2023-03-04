bad67dda037ed853f8681b0332985212405df999
theJoseNeto <joseneto180201@gmail.com>
2023-03-04 02:08:31 -0300

    variaveis e operadores

--- fundamentos/aula03/Program.cs
+++ fundamentos/aula03/Program.cs
@@ -7,20 +7,20 @@ namespace aula03
         static void Main(string[] args){
             //variáveis e seus tipos mais primitivos
             int num = 1; 
-            char letter = "w";
+            char letter = 'w';
             float pi = 3.14f; 
             string name = "Neto"; 
             var aux = name; 
 
             // constantes 
-            const int num = 1; 
-            const char letter = "w";
-            const float pi = 3.14f; 
-            const string name = "Neto"; 
-            const var aux = name; 
+            const int num2 = 1; 
+            const char letter2 = 'w';
+            const float pi2 = 3.14f; 
+            const string name2 = "Neto"; 
+            // const var aux = name; 
 
             // Declaração de multiplas variáveis com mesmo tipo; 
-            int num2, num3, num4;
+            int num21, num3, num4;
             char teste1, teste2, teste3; 
             // ... 
 
